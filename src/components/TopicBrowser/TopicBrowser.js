6a1c0d0449084c0b788126cbda7b0b642abd2ed9
Meke Finau <lonifinau.09@gmail.com>
2022-04-26 12:51:05 -0600

    even and odd

--- src/components/TopicBrowser/TopicBrowser.js
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -1,20 +1,20 @@
-import React from "react";
-import EvenAndOdd from "../Topics/EvenAndOdd";
-import FilterObject from "../Topics/FilterObject";
-import FilterString from "../Topics/FilterString";
-import Palindrome from "../Topics/Palindrome";
-import Sum from "../Topics/Sum";
+import React from 'react';
+import EvenAndOdd from '../Topics/EvenAndOdd';
+import FilterObject from '../Topics/FilterObject';
+import FilterString from '../Topics/FilterString';
+import Palindrome from '../Topics/Palindrome';
+import Sum from '../Topics/Sum';
 
 const TopicBrowser = () => {
-    return (
-        <div className="topic-browser">
-            <EvenAndOdd />
-            <FilterObject />
-            <FilterString />
-            <Palindrome />
-            <Sum />
-        </div>
-    );
+  return (
+    <div className='puzzleFeed'>
+      <EvenAndOdd />
+      <FilterObject />
+      <FilterString />
+      <Palindrome />
+      <Sum />
+    </div>
+  );
 };
 
 export default TopicBrowser;
--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,21 +1,45 @@
-import React from "react";
-import { useState } from "react/cjs/react.production.min";
+import React, { Component } from 'react';
 
-const EvenAndOdd = (props) => {
-    const evenArray = useState([]);
-    const oddArray = useState([]);
-    const userInput = useState("");
+export default class EvenAndOdd extends Component {
+  constructor() {
+    super();
+    this.state = {
+      evenArray: [],
+      oddArray: [],
+      userInput: '',
+    };
+  }
 
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+  assignEvenAndOdds(userInput) {
+    var arr = userInput.split(',');
+    var evens = [];
+    var odds = [];
+
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        evens.push(parseInt(arr[i], 10));
+      } else {
+        odds.push(parseInt(arr[i], 10));
+      }
+    }
+
+    this.setState({ evenArray: evens, oddArray: odds });
+  }
+
+  render() {
     return (
-        <div className="even-and-odds">
-            <h4>Even and Odd</h4>
-            <div className="puzzleBox"></div>
-            <input className="inputLine"></input>
-            <button className="confirmationButton"></button>
-            <span className="resultBox"></span>
-            <span className="resultBox"></span>
-        </div>
+      <div className='even-and-odds'>
+        <h4>Even and Odd</h4>
+        <div className='puzzleBox'></div>
+        <input className='inputLine'></input>
+        <button className='confirmationButton'></button>
+        <span className='resultBox'></span>
+        <span className='resultBox'></span>
+      </div>
     );
-};
-
-export default EvenAndOdd;
+  }
+}
